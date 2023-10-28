d8c2cfc37a404b9b7509393becf4c5f2683c7158
Ambarish <asg1898@gmail.com>
2023-10-28 11:29:42 +0530

    Exercise 4 solution

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -1,9 +1,21 @@
 import boto3
+import os
+import pandas as pd
+import json
 
 
 def main():
-    # your code here
-    pass
+    for root, dirs, files in os.walk('data'):
+        json_files = list(filter(lambda file: file.endswith('.json'), files))
+        print(json_files)
+        for json_file in json_files:
+            print(f"{root}\{json_file}")
+            with open(f"{root}\{json_file}") as file:
+                data = json.load(file)
+                # normalize doesn't seem to be working for the example `{"type":"Point","coordinates":[-99.9,16.88333]}` value
+                # beyond the first level i.e., geolocation.type: "Point" geolocation.coordinates: [lat, long]
+                json_file_normal = pd.json_normalize(data, max_level=3)
+                json_file_normal.to_csv(f"{root}\{json_file.replace('.json', '.csv')}", index=False)
 
 
 if __name__ == "__main__":
